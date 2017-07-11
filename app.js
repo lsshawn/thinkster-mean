7be95b68323ae3dbc73e495e57f1e34a6a549277
lsshawn <coffee.or.tea88@gmail.com>
2017-07-11 08:06:55 +0000

    Add ng-template

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function() {
   var o = {
@@ -11,30 +26,41 @@ app.controller('MainCtrl', [
   '$scope',
   'posts',
   function($scope, posts) {
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 19},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
-   
+    $scope.posts = [{
+      title: 'post 1',
+      upvotes: 5
+    }, {
+      title: 'post 2',
+      upvotes: 2
+    }, {
+      title: 'post 3',
+      upvotes: 19
+    }, {
+      title: 'post 4',
+      upvotes: 9
+    }, {
+      title: 'post 5',
+      upvotes: 4
+    }];
+
     $scope.addPost = function() {
-      if(!$scope.title || $scope.title === '') { return; }
-      
+      if (!$scope.title || $scope.title === '') {
+        return;
+      }
+
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
         upvotes: 0
       });
-      
+
       $scope.title = '';
       $scope.link = '';
     }
-    
+
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
     }
-    
+
   }
 ]);
